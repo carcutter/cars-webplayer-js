7298ce95406cb12c92dbe1176fea4e293095dbd3
Rémi Lux <remi.lux@car-cutter.com>
2024-06-26 09:52:30 +0200

    Plug video element

--- src/components/atoms/media_elements/VideoElement.tsx
+++ src/components/atoms/media_elements/VideoElement.tsx
@@ -1,9 +1,28 @@
 import { Item } from "@/types/composition";
+import { useState } from "react";
 
 type Props = { item: Extract<Item, { type: "video" }> };
 
 const VideoElement: React.FC<Props> = ({ item: { src, poster } }) => {
-  return <img key={src} className="size-full" src={poster} alt="" />;
+  const [displayVideo, setDisplayVideo] = useState(false);
+
+  return (
+    <div className="relative size-full">
+      {!displayVideo ? (
+        <>
+          <img key={src} className="size-full" src={poster} alt="" />
+          <div
+            className="absolute flex justify-center items-center inset-0 bg-foreground/25 cursor-pointer"
+            onClick={() => setDisplayVideo(true)}
+          >
+            <div className="text-background">PLAY</div>
+          </div>
+        </>
+      ) : (
+        <video className="size-full" src={src} controls autoPlay />
+      )}
+    </div>
+  );
 };
 
 export default VideoElement;
