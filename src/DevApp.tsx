38d5b946577671b573245ad2adfe5d683641bf4d
Rémi Lux <remi.lux@car-cutter.com>
2024-07-03 09:21:06 +0200

    Move DevApp to improve HMR

---
+++ src/DevApp.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+
+import WebPlayer from "./custom_elements/WebPlayer.tsx";
+import WebPlayerIcon from "./custom_elements/WebPlayerIcon.tsx";
+import { DEFAULT_EVENT_ID } from "./types/props.ts";
+
+const DevApp: React.FC = () => {
+  useEffect(() => {
+    const onEvent = (e: Event) => {
+      const { detail } = e as CustomEvent;
+
+      // eslint-disable-next-line no-console
+      console.log("Event received:", detail);
+    };
+
+    // Listen for the custom event
+    document.addEventListener(DEFAULT_EVENT_ID, onEvent);
+
+    return () => {
+      document.removeEventListener(DEFAULT_EVENT_ID, onEvent);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* FUTURE: Add some stuff to make it appear like a real app */}
+
+      <div
+        style={{
+          padding: "1rem",
+          marginBottom: "1rem",
+          borderBottom: "1px solid #000",
+        }}
+      >
+        <h2
+          style={{
+            fontSize: "1.5rem",
+          }}
+        >
+          Carcutter Demo React
+        </h2>
+      </div>
+
+      <WebPlayer
+        compositionUrl="/data.json"
+        aspectRatio="4:3"
+        maxItemsShown={2.5}
+        flatten
+      >
+        <WebPlayerIcon feature="DRIVER ASSIST" color="#ff00ff">
+          <svg
+            style={{ color: "white" }}
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 -960 960 960"
+            width="48"
+          >
+            <path
+              fill="currentColor"
+              d="M230-450q-12.75 0-21.375-8.675-8.625-8.676-8.625-21.5 0-12.825 8.625-21.325T230-510h500q12.75 0 21.375 8.675 8.625 8.676 8.625 21.5 0 12.825-8.625 21.325T730-450H230Z"
+            />
+          </svg>
+        </WebPlayerIcon>
+      </WebPlayer>
+    </div>
+  );
+};
+
+export default DevApp;
