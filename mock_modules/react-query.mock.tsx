2de0b7a6a8d92c79cb92cb4ba5411fbf34dcd3ba
Rémi Lux <remi.lux@car-cutter.com>
2024-07-11 11:01:57 +0200

    Add cache to react-query mock

--- mock_modules/react-query.mock.tsx
+++ mock_modules/react-query.mock.tsx
@@ -1,25 +1,60 @@
-import type { useQuery, QueryObserverResult } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+/**
+ * This file mocks the react-query library to provide a simplified version of the library and reduce the bundle size.
+ * Unexpected behavior may occur
+ *
+ * Not implemented:
+ * - Cache invalidation (e.g. refetching/updating queries data). But as our data is static, we don't need it.
+ */
 
-type MockQueryObserverResult<TData, TError> = Pick<
-  QueryObserverResult<TData, TError>,
-  "status" | "data" | "isSuccess" | "error"
->;
+import type { useQuery, QueryObserverResult } from "@tanstack/react-query";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 
 class QueryClient {
   constructor() {}
 }
 
+type ContextType = {
+  cache: Map<string, unknown | Promise<unknown>>;
+};
+
+const QueryClientContext = createContext<ContextType | null>(null);
+
+export const useQueryClientContext = () => {
+  const ctx = useContext(QueryClientContext);
+
+  if (!ctx) {
+    throw new Error(
+      "useQueryClientContext must be used within a QueryClientProvider"
+    );
+  }
+
+  return ctx;
+};
+
 const QueryClientProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  return children;
+  const cacheRef = useRef(new Map<string, unknown | Promise<unknown>>());
+
+  return (
+    <QueryClientContext.Provider value={{ cache: cacheRef.current }}>
+      {children}
+    </QueryClientContext.Provider>
+  );
 };
+
+// Extract the attributes we use from React-Query
+type MockQueryObserverResult<TData, TError> = Pick<
+  QueryObserverResult<TData, TError>,
+  "status" | "data" | "isSuccess" | "error"
+>;
 
 function useQueryMock<TData = unknown, TError = unknown>(
   options: Parameters<typeof useQuery<TData, TError>>[0]
 ): MockQueryObserverResult<TData, TError> {
   const { queryKey, queryFn } = options;
+
+  const { cache } = useQueryClientContext();
 
   const [state, setState] = useState<MockQueryObserverResult<TData, TError>>({
     status: "pending",
@@ -35,23 +70,50 @@ function useQueryMock<TData = unknown, TError = unknown>(
       return;
     }
 
-    (async () => {
-      setState({
-        status: "pending",
-        data: undefined,
-        isSuccess: false,
-        error: null,
-      });
+    const setSuccess = (data: TData) =>
+      setState({ status: "success", data, isSuccess: true, error: null });
 
+    const cachedValue = cache.get(queryKeyStr) as
+      | TData
+      | Promise<TData>
+      | undefined;
+
+    // If the cached value is already resolved, set it directly
+    if (cachedValue && !(cachedValue instanceof Promise)) {
+      setSuccess(cachedValue);
+      return;
+    }
+
+    setState({
+      status: "pending",
+      data: undefined,
+      isSuccess: false,
+      error: null,
+    });
+
+    const controller = new AbortController();
+
+    // Run the query
+    (async function () {
       try {
-        const controller = new AbortController();
+        let data: TData;
 
-        const data = await queryFn({
-          ...options,
-          signal: controller.signal,
-          meta: undefined,
-        });
-        setState({ status: "success", data, isSuccess: true, error: null });
+        // using cached promise
+        if (cachedValue) {
+          data = await cachedValue;
+        } else {
+          const promise = queryFn({
+            ...options,
+            signal: controller.signal,
+            meta: undefined,
+          });
+          cache.set(queryKeyStr, promise);
+
+          data = await promise;
+          cache.set(queryKeyStr, data);
+        }
+
+        setSuccess(data);
       } catch (err) {
         setState({
           status: "error",
@@ -61,6 +123,10 @@ function useQueryMock<TData = unknown, TError = unknown>(
         });
       }
     })();
+
+    return () => {
+      controller.abort();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [queryKeyStr]);
 
