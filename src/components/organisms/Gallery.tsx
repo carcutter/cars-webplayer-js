fb4f64381179c28dcebd0185927f2a02ba7e3a47
Rémi Lux <remi.lux@car-cutter.com>
2024-07-31 12:09:47 +0200

    Gallery is now draggable

--- src/components/organisms/Gallery.tsx
+++ src/components/organisms/Gallery.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import GalleryElement from "@/components/molecules/GalleryElement";
 import { useCompositionContext } from "@/providers/CompositionContext";
@@ -41,6 +41,12 @@ const Gallery: React.FC<Props> = ({
     return sliderRef.current;
   }, []);
 
+  const mouseIsDown = useRef(false);
+  const sliderStartSnapshot = useRef<{ x: number; scrollLeft: number } | null>(
+    null
+  );
+  const [isDragging, setIsDragging] = useState(false);
+
   const slideToIndex = useCallback(
     (index: number, behavior: "instant" | "smooth") => {
       const slider = getSliderOrThrow();
@@ -61,6 +67,76 @@ const Gallery: React.FC<Props> = ({
   );
 
   // -- Effects
+  // - Event listeners to handle the slider dragging -- //
+  useEffect(() => {
+    const slider = sliderRef.current;
+
+    // DOM not ready yet
+    if (!slider) {
+      return;
+    }
+
+    // Handle when the user just clicked on the slider
+    const onMouseDown = (e: MouseEvent) => {
+      e.preventDefault(); // Prevents native text selection (which highlights images)
+
+      // Take snapshot of the starting state
+      mouseIsDown.current = true;
+      sliderStartSnapshot.current = {
+        x: e.pageX - slider.offsetLeft,
+        scrollLeft: slider.scrollLeft,
+      };
+    };
+
+    // Scroll according the user's dragging movement
+    const onMouseMove = (e: MouseEvent) => {
+      // Check if the user is actually dragging
+      if (!mouseIsDown.current) {
+        return;
+      }
+
+      if (sliderStartSnapshot.current === null) {
+        throw new Error("[onMouseMove] scrollStart is null");
+      }
+
+      const x = e.pageX - slider.offsetLeft;
+      const walk = x - sliderStartSnapshot.current.x;
+      const newScrollLeft = sliderStartSnapshot.current.scrollLeft - walk;
+
+      setIsDragging(true);
+
+      requestAnimationFrame(() => {
+        slider.scrollLeft = newScrollLeft;
+      });
+    };
+
+    // Handle when the user releases the slider or leaves the dragging area
+    const onMouseEnd = () => {
+      // Check if the user was actually dragging
+      if (!mouseIsDown.current) {
+        return;
+      }
+
+      mouseIsDown.current = false;
+
+      // As the mouseup event is triggered before the click event, we need to wait a bit before resetting the dragging state
+      setTimeout(() => {
+        setIsDragging(false);
+      }, 0);
+    };
+
+    slider.addEventListener("mousedown", onMouseDown);
+    slider.addEventListener("mouseleave", onMouseEnd);
+    slider.addEventListener("mouseup", onMouseEnd);
+    slider.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+      slider.removeEventListener("mousedown", onMouseDown);
+      slider.removeEventListener("mouseleave", onMouseEnd);
+      slider.removeEventListener("mouseup", onMouseEnd);
+      slider.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
 
   // Ref to use index within effect without re-rendering
   const lastMasterItemIndexRef = useRef(masterItemIndex);
@@ -97,6 +173,11 @@ const Gallery: React.FC<Props> = ({
   ]);
 
   const onItemClicked = (_item: Item, index: number) => {
+    // User is dragging the slider, ignore the click
+    if (isDragging) {
+      return;
+    }
+
     setItemIndexCommand(index);
     resetView();
   };
@@ -104,7 +185,7 @@ const Gallery: React.FC<Props> = ({
   return (
     <div
       ref={sliderRef}
-      className={`relative h-12 w-full overflow-x-auto no-scrollbar ${!extendMode ? "" : "sm:h-20"} ${className}`}
+      className={`relative h-12 w-full overflow-x-auto no-scrollbar ${!extendMode ? "" : "sm:h-20"} ${!isDragging ? "cursor-grab" : "cursor-grabbing"} ${className}`}
     >
       <div
         className={`flex h-full w-fit gap-2 ${!extendMode ? "" : "sm:gap-4"} ${containerClassName}`}
@@ -113,7 +194,7 @@ const Gallery: React.FC<Props> = ({
           <div
             key={index}
             className={
-              `relative h-full ${aspectRatioClass} cursor-pointer` +
+              `relative h-full ${aspectRatioClass} ${!isDragging ? "cursor-pointer" : ""}` +
               ` after:absolute after:inset-0 after:border-2 after:border-primary after:transition-opacity ${index === masterItemIndex ? "after:opacity-100" : "after:opacity-0 hover:after:opacity-70"}`
             }
             onClick={() => onItemClicked(item, index)}
