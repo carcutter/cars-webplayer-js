732da3da961ee5f9b3a31cd4e643ccd8523c50d1
Rémi Lux <remi.lux@car-cutter.com>
2024-08-05 09:28:29 +0200

    Remove padding on Full-screen

--- src/components/organisms/WebPlayerContainer.tsx
+++ src/components/organisms/WebPlayerContainer.tsx
@@ -89,7 +89,7 @@ const WebPlayerContent: React.FC<React.PropsWithChildren> = () => {
 
   return (
     <div
-      // Main Overlay (apply backdrop)
+      // Main Overlay (apply backdrop + close button)
       className={`relative ${!extendMode ? "" : "flex size-full items-center justify-center bg-foreground/75"}`}
       onClick={handleCloseElementClick}
     >
@@ -98,7 +98,7 @@ const WebPlayerContent: React.FC<React.PropsWithChildren> = () => {
         className={
           !extendMode
             ? "space-y-2"
-            : "flex size-full flex-col justify-center gap-y-2 py-16 sm:gap-y-4"
+            : "flex size-full flex-col justify-center gap-y-2 sm:gap-y-4"
         }
         onClick={handleCloseElementClick}
       >
@@ -116,7 +116,7 @@ const WebPlayerContent: React.FC<React.PropsWithChildren> = () => {
           />
         </div>
         {permanentGallery && (
-          <Gallery className={!extendMode ? undefined : "shrink-0"} />
+          <Gallery className={!extendMode ? undefined : "my-4 shrink-0"} />
         )}
       </div>
 
