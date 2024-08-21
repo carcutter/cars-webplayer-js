d68ede8f734ba8c65d4dda88584c8ea3af7cbc7f
Rémi Lux <remi.lux@car-cutter.com>
2024-08-21 10:11:12 +0200

    Update vue adapter

--- packages/webplayer-vue/src/WebPlayer.vue
+++ packages/webplayer-vue/src/WebPlayer.vue
@@ -1,25 +1,24 @@
-<script setup>
+<script lang="ts" setup>
+import type { WebPlayerProps } from "@car-cutter/react-webplayer";
 import { ensureCustomElementsDefinition } from "@car-cutter/wc-webplayer";
 
 ensureCustomElementsDefinition();
+
+defineProps<WebPlayerProps>();
 </script>
 
 <template>
-  <cc-webplayer :composition-url="compositionUrl" :flatten="flatten">
+  <cc-webplayer
+    :composition-url="compositionUrl"
+    :reverse360="reverse360"
+    :min-image-width="minImageWidth"
+    :max-image-width="maxImageWidth"
+    :image-load-strategy="imageLoadStrategy"
+    :flatten="flatten"
+    :infinite-carrousel="infiniteCarrousel"
+    :event-id="eventId"
+    :allow-full-screen="allowFullScreen"
+    :permanent-gallery="permanentGallery"
+  >
   </cc-webplayer>
 </template>
-
-<script>
-export default {
-  name: "WebPlayer",
-  props: {
-    compositionUrl: {
-      type: String,
-      required: true,
-    },
-    flatten: {
-      type: Boolean,
-    },
-  },
-};
-</script>
