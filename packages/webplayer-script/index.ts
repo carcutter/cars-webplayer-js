03b7a298cceeae7cf53a9005e2042a4c9d1e230e
Rémi Lux <remi.lux@car-cutter.com>
2024-08-21 14:33:09 +0200

    Create WebPlayer callable with <script/>

---
+++ packages/webplayer-script/index.ts
@@ -0,0 +1,4 @@
+import { defineCustomElements } from "@car-cutter/wc-webplayer";
+
+// Users want custom elements to be defined without having to call an extra function
+defineCustomElements();
---
+++ packages/webplayer-script/vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  define: {
+    "process.env": {
+      NODE_ENV: "production",
+    },
+  },
+  build: {
+    lib: {
+      name: "script-webplayer",
+      fileName: () => "bundle.js",
+      entry: resolve(__dirname, "./index.ts"),
+      formats: ["umd"],
+    },
+  },
+});
