726699e4d27bbc7667880648488631f14f139574
Rémi Lux <remi.lux@car-cutter.com>
2024-08-22 11:11:25 +0200

    trailingComma prettier (for cn function)

--- packages/webplayer-react/src/components/organisms/WebPlayerContainer.tsx
+++ packages/webplayer-react/src/components/organisms/WebPlayerContainer.tsx
@@ -134,7 +134,7 @@ const WebPlayerContent: React.FC<React.PropsWithChildren> = () => {
         !extendMode
           ? "relative"
           : "flex size-full items-center justify-center bg-foreground/75",
-        fakeFullScreen && "fixed inset-0",
+        fakeFullScreen && "fixed inset-0"
       )}
     >
       <div
