146b85853fa6ed1a5bb6442dd378412f02119327
Rémi Lux <remi.lux@car-cutter.com>
2024-09-20 17:09:06 +0200

    Fix DEFAULT props typing .d.ts

--- packages/core/src/const/default_props.ts
+++ packages/core/src/const/default_props.ts
@@ -1,11 +1,12 @@
 import { ImageLoadStrategy } from "../types/misc";
 
-export const DEFAULT_HIDE_CATEGORIES: boolean = false;
-export const DEFAULT_INFINITE_CARROUSEL: boolean = false;
-export const DEFAULT_PERMANENT_GALLERY: boolean = false;
+export const DEFAULT_HIDE_CATEGORIES = false satisfies boolean;
+export const DEFAULT_INFINITE_CARROUSEL = false satisfies boolean;
+export const DEFAULT_PERMANENT_GALLERY = false satisfies boolean;
 
-export const DEFAULT_IMAGE_LOAD_STRATEGY: ImageLoadStrategy = "quality";
+export const DEFAULT_IMAGE_LOAD_STRATEGY =
+  "quality" satisfies ImageLoadStrategy;
 
-export const DEFAULT_PREVENT_FULL_SCREEN: boolean = false;
-export const DEFAULT_EVENT_PREFIX: string = "cc-webplayer:";
-export const DEFAULT_REVERSE_360: boolean = false;
+export const DEFAULT_PREVENT_FULL_SCREEN = false satisfies boolean;
+export const DEFAULT_EVENT_PREFIX = "cc-webplayer:" satisfies string;
+export const DEFAULT_REVERSE_360 = false satisfies boolean;
