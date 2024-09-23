158e81b5b68e733bb2c62af1ad132f6890efce55
Rémi Lux <remi.lux@car-cutter.com>
2024-09-23 09:48:34 +0200

    Improve useSearchParam hook

--- apps/showroom/src/hooks/useSearchParam.ts
+++ apps/showroom/src/hooks/useSearchParam.ts
@@ -3,37 +3,52 @@ import { useCallback, useState } from "react";
 export function useSearchParam<T extends string | boolean = string>(
   key: string,
   defaultValue: T = "" as T
-): [T, (v: T) => void] {
+): [T, (v: T) => void, boolean] {
   const queryParameters = new URLSearchParams(window.location.search);
+  const queryParam = queryParameters.get(key);
+
+  const [isDefault, setIsDefault] = useState(!queryParam);
 
   const [value, setValue] = useState<T>(() => {
-    const v = queryParameters.get(key);
-
-    if (!v) {
+    if (!queryParam) {
       return defaultValue;
     }
 
     if (typeof defaultValue === "boolean") {
-      return (v === "true") as T;
+      return (queryParam === "true") as T;
     }
 
-    return v as T;
+    return queryParam as T;
   });
 
   const set = useCallback(
     (v: T) => {
       setValue(v);
+      setIsDefault(false);
+
       const newParams = new URLSearchParams(window.location.search);
-      newParams.set(key, v.toString());
+
+      const vString = v.toString();
+
+      if (!vString) {
+        newParams.delete(key);
+      } else {
+        newParams.set(key, v.toString());
+      }
+
+      const urlSegments = [window.location.pathname];
+
+      const newParamsString = newParams.toString();
+
+      if (newParamsString) {
+        urlSegments.push(newParamsString);
+      }
+
       // FUTURE: use pushState & subscribe to update params (especially for back/forward)
-      window.history.replaceState(
-        {},
-        "",
-        `${window.location.pathname}?${newParams}`
-      );
+      window.history.replaceState({}, "", urlSegments.join("?"));
     },
     [key]
   );
 
-  return [value, set];
+  return [value, set, isDefault];
 }
