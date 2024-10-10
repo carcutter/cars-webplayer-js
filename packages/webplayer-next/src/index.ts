f707b7d252dfb6ef965e1c6bd14f36cd9ea7f4cd
Rémi Lux <remi.lux@car-cutter.com>
2024-10-10 17:23:11 +0200

    Add "shared-exports"

--- packages/webplayer-next/src/index.ts
+++ packages/webplayer-next/src/index.ts
@@ -2,34 +2,4 @@ export { WebPlayer } from "./WebPlayer";
 export { WebPlayerCustomMedia } from "./WebPlayerCustomMedia";
 export { WebPlayerIcon } from "./WebPlayerIcon";
 
-// -- Expose ancestors package
-export {
-  // - Components
-  WEB_PLAYER_WC_TAG,
-  WEB_PLAYER_CUSTOM_MEDIA_WC_TAG,
-  WEB_PLAYER_ICON_WC_TAG,
-  // - Events
-  DEFAULT_EVENT_PREFIX,
-  EVENT_COMPOSITION_LOADING,
-  EVENT_COMPOSITION_LOADED,
-  EVENT_COMPOSITION_LOAD_ERROR,
-  EVENT_ITEM_CHANGE,
-  EVENT_EXTEND_MODE_ON,
-  EVENT_EXTEND_MODE_OFF,
-  EVENT_HOTSPOTS_ON,
-  EVENT_HOTSPOTS_OFF,
-  EVENT_GALLERY_OPEN,
-  EVENT_GALLERY_CLOSE,
-  // - Utils
-  generateCompositionUrl,
-  // - Types
-  type Item,
-  type Composition,
-  type MediaLoadStrategy,
-} from "@car-cutter/core";
-
-export type {
-  WebPlayerProps,
-  WebPlayerCustomMediaProps,
-  WebPlayerIconProps,
-} from "@car-cutter/react-webplayer";
+export * from "./shared-exports";
---
+++ packages/webplayer-react/src/shared-exports.ts
@@ -0,0 +1,34 @@
+export { WebPlayer, type WebPlayerProps } from "./components/WebPlayer";
+export {
+  WebPlayerCustomMedia,
+  type WebPlayerCustomMediaProps,
+} from "./components/WebPlayerCustomMedia";
+export {
+  WebPlayerIcon,
+  type WebPlayerIconProps,
+} from "./components/WebPlayerIcon";
+
+// -- Expose ancestors package
+export {
+  // - Components
+  WEB_PLAYER_WC_TAG,
+  WEB_PLAYER_ICON_WC_TAG,
+  // - Events
+  DEFAULT_EVENT_PREFIX,
+  EVENT_COMPOSITION_LOADING,
+  EVENT_COMPOSITION_LOADED,
+  EVENT_COMPOSITION_LOAD_ERROR,
+  EVENT_ITEM_CHANGE,
+  EVENT_EXTEND_MODE_ON,
+  EVENT_EXTEND_MODE_OFF,
+  EVENT_HOTSPOTS_ON,
+  EVENT_HOTSPOTS_OFF,
+  EVENT_GALLERY_OPEN,
+  EVENT_GALLERY_CLOSE,
+  // - Utils
+  generateCompositionUrl,
+  // - Types
+  type Item,
+  type Composition,
+  type MediaLoadStrategy,
+} from "@car-cutter/core";
