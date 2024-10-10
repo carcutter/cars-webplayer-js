74cd3a32a4a19500b23e4bad992bc6b84033cb57
Rémi Lux <remi.lux@car-cutter.com>
2024-10-10 11:49:50 +0200

    Only build CSS in core-ui

--- packages/core-ui/vite.config.ts
+++ packages/core-ui/vite.config.ts
@@ -1,18 +1,9 @@
-import { resolve } from "path";
-
 import react from "@vitejs/plugin-react";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    dts({
-      tsconfigPath: resolve(__dirname, "./tsconfig.app.json"),
-      rollupTypes: true,
-    }),
-  ],
+  plugins: [react()],
 
   // BUILD
   define: {
@@ -22,23 +13,19 @@ export default defineConfig(({ mode }) => ({
   },
   // FUTURE: Find a way to build in watch mode. The simple script "watch": "vite build --watch" does not work because it does not rebuild the TS.
   build: {
+    cssCodeSplit: true,
     lib: {
-      name: "CarCutterCoreUI",
-      fileName: "index",
-      entry: resolve(__dirname, "./index.ts"),
+      entry: "src/index.css",
+      formats: ["es"],
+      fileName: "style",
     },
     target: browserslistToEsbuild(),
 
     copyPublicDir: false, // The only public file is mock data
 
     rollupOptions: {
-      external: ["@car-cutter/core", "react", "react/jsx-runtime"],
       output: {
-        globals: {
-          "@car-cutter/core": "CarCutterCore",
-          react: "React",
-          "react/jsx-runtime": "jsx",
-        },
+        assetFileNames: "style.[ext]",
       },
     },
   },
