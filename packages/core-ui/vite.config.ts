254790fc7226b34dcf86ca83b6b5c2e215c76516
Rémi Lux <remi.lux@car-cutter.com>
2024-10-22 10:37:31 +0200

    Add preact dev

--- packages/core-ui/vite.config.ts
+++ packages/core-ui/vite.config.ts
@@ -1,32 +1,45 @@
+import preact from "@preact/preset-vite";
 import react from "@vitejs/plugin-react";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isPreactDev = mode === "development-preact";
 
-  // BUILD
-  define: {
-    "process.env": {
-      NODE_ENV: mode,
-    },
-  },
-  // FUTURE: Find a way to build in watch mode. The simple script "watch": "vite build --watch" does not work because it does not rebuild the TS.
-  build: {
-    cssCodeSplit: true,
-    lib: {
-      entry: "src/index.css",
-      formats: ["es"],
-      fileName: "style",
-    },
-    target: browserslistToEsbuild(),
+  return {
+    plugins: [isPreactDev ? preact() : react()],
 
-    copyPublicDir: false, // The only public file is mock data
-
-    rollupOptions: {
-      output: {
-        assetFileNames: "style.[ext]",
+    define: {
+      "process.env": {
+        NODE_ENV: mode,
       },
     },
-  },
-}));
+    // FUTURE: Find a way to build in watch mode. The simple script "watch": "vite build --watch" does not work because it does not rebuild the TS.
+    build: {
+      cssCodeSplit: true,
+      lib: {
+        entry: "src/index.css",
+        formats: ["es"],
+        fileName: "style",
+      },
+      target: browserslistToEsbuild(),
+
+      copyPublicDir: false, // The only public file is mock data
+
+      rollupOptions: {
+        output: {
+          assetFileNames: "style.[ext]",
+        },
+      },
+    },
+
+    resolve: {
+      alias: isPreactDev
+        ? {
+            react: "preact/compat",
+            "react-dom": "preact/compat",
+          }
+        : undefined,
+    },
+  };
+});
