01880f4d8b2af44a724d1f39ed1aa5b7deacb07f
Evgenii Sergeev <evgenii.sergeev@diffuse.ly>
2025-04-07 10:52:53 +0200

    refactor: :art: Add custom type defentions for pannellum.

---
+++ packages/core-ui/src/types/declarations/pannellum-react.d.ts
@@ -0,0 +1,41 @@
+declare module "pannellum-react" {
+  import { Component, ReactNode } from "react";
+
+  export interface PannellumProps {
+    id?: string;
+    width?: string;
+    height?: string;
+    image?: string;
+    panorama?: string;
+    preview?: string;
+    pitch?: number;
+    yaw?: number;
+    hfov?: number;
+    maxHfov?: number;
+    minHfov?: number;
+    autoLoad?: boolean;
+    compass?: boolean;
+    showZoomCtrl?: boolean;
+    showFullscreenCtrl?: boolean;
+    showControls?: boolean;
+    onLoad?: () => void;
+    onError?: () => void;
+    onMousedown?: (e: Event) => void;
+    onMouseup?: (e: Event) => void;
+    onTouchstart?: (e: Event) => void;
+    onTouchend?: (e: Event) => void;
+    children?: ReactNode;
+  }
+
+  export interface PannellumViewer {
+    loadScene: () => void;
+    on?: (eventName: string, callback: (progress: number) => void) => void;
+    off?: (eventName: string, callback: (progress: number) => void) => void;
+  }
+
+  export class Pannellum extends Component<PannellumProps> {
+    getViewer(): PannellumViewer;
+
+    static Controls: React.FC<{ children: ReactNode }>;
+  }
+}
