99948641e979dc1d1ae57309c31d2140b6b403f8
Evgenii Sergeev <56795482+evgenii-sergeev@users.noreply.github.com>
2025-04-11 12:31:46 +0200

    Fix build (#21)
    * fix: :green_heart: Add global type declaration, remove shadow dom and reconfigure bundler.
    
    * refactor: :fire: Simplified WebPlayerWebComponent shadow config.

--- packages/core-ui/vite.config.ts
+++ packages/core-ui/vite.config.ts
@@ -1,13 +1,34 @@
+import fs from "fs";
+import { resolve } from "path";
+
 import preact from "@preact/preset-vite";
 import react from "@vitejs/plugin-react";
 import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
 
+const cssExtractPlugin = () => {
+  return {
+    name: "css-extract",
+    closeBundle: {
+      sequential: true,
+      handler() {
+        const dir = "dist";
+        const files = fs.readdirSync(dir);
+        for (const file of files) {
+          if (!file.endsWith(".css")) {
+            fs.unlinkSync(resolve(dir, file));
+          }
+        }
+      },
+    },
+  };
+};
+
 export default defineConfig(({ mode }) => {
   const isPreactDev = mode === "development-preact";
 
   return {
-    plugins: [isPreactDev ? preact() : react()],
+    plugins: [isPreactDev ? preact() : react(), cssExtractPlugin()],
 
     define: {
       "process.env": {
@@ -17,20 +38,16 @@ export default defineConfig(({ mode }) => {
     // FUTURE: Find a way to build in watch mode. The simple script "watch": "vite build --watch" does not work because it does not rebuild the TS.
     build: {
       cssCodeSplit: true,
-      lib: {
-        entry: "src/index.css",
-        formats: ["es"],
-        fileName: "style",
-      },
-      target: browserslistToEsbuild(),
-
-      copyPublicDir: false, // The only public file is mock data
-
+      outDir: "dist",
       rollupOptions: {
+        input: "index.ts",
         output: {
           assetFileNames: "style.[ext]",
+          entryFileNames: "[name].js",
         },
       },
+      target: browserslistToEsbuild(),
+      copyPublicDir: false,
     },
 
     resolve: {
