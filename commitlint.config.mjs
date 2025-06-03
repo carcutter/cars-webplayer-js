daeaa2ce8e9764068a2d22f4086b902404e9a2a7
Markus Füreder <markus.fuereder@car-cutter.com>
2025-06-03 09:45:25 +0200

    chore: Update commitlint config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -4,7 +4,7 @@ const config = {
   rules: {
     "body-leading-blank": [1, "always"],
     "body-max-line-length": [0, "always", 100],
-    "footer-leading-blank": [0, "always"],
+    "footer-leading-blank": [1, "always"],
     "footer-max-line-length": [2, "always", 100],
     "header-max-length": [2, "always", 100],
     "subject-case": [2, "always", ["sentence-case"]],
