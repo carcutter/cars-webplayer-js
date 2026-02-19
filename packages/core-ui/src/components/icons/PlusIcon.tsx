1632a6853c22bdb8f0e9b9a4e8f18f443d4b227a
Evgenii Sergeev <evgenii.sergeev@diffuse.ly>
2026-02-19 17:18:46 +0100

    feat: ✨ Update icon components

--- packages/core-ui/src/components/icons/PlusIcon.tsx
+++ packages/core-ui/src/components/icons/PlusIcon.tsx
@@ -5,17 +5,13 @@ type Props = { className?: string };
 const PlusIcon: React.FC<Props> = ({ className }) => {
   return (
     <CustomizableIcon className={className} customizationKey="UI_PLUS">
-      <svg
-        className={className}
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg className={className} viewBox="0 0 13 13" fill="none">
         <path
-          d="M12.6666 8.66634H8.66659V12.6663H7.33325V8.66634H3.33325V7.33301H7.33325V3.33301H8.66659V7.33301H12.6666V8.66634Z"
-          fill="currentColor"
+          d="M0.520874 6.35409H12.1875M6.35421 0.520752V12.1874"
+          stroke="currentColor"
+          strokeWidth="1.04167"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </CustomizableIcon>
